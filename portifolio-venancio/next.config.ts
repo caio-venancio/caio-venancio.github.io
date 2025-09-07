d182e6418fb30cd65bbe92e9483db66f45ae6a86
caio-venancio <caio.venancio784@gmail.com>
2025-09-07 12:51:11 -0300

    feat: deploy estático com next.js

--- portifolio-venancio/next.config.ts
+++ portifolio-venancio/next.config.ts
@@ -1,22 +1,22 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-const repo = '/meu-jogo'; //nome do repositório
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',           // gera HTML/CSS/JS estático em /out
-  images: { unoptimized: true }, // evita otimização que exige servidor
-  // opcional: se usar rotas além de '/', pode querer URLs com /
-  // trailingSlash: true,
-  basePath: repo,
-  assetPrefix: repo + '/',
-  trailingSlash: true, // ajuda no GitHub Pages a servir /rota/index.html
+const nextConfig: NextConfig = {
+  /* config options here */
 };
+
 export default nextConfig;
+
+// const repo = '/meu-jogo'; //nome do repositório
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',           // gera HTML/CSS/JS estático em /out
+//   images: { unoptimized: true }, // evita otimização que exige servidor
+//   // opcional: se usar rotas além de '/', pode querer URLs com /
+//   // trailingSlash: true,
+//   // basePath: repo,
+//   // assetPrefix: repo + '/',
+//   // trailingSlash: true, // ajuda no GitHub Pages a servir /rota/index.html
+// };
+// export default nextConfig;
 
--- portifolio-venancio/src/app/page.tsx
+++ portifolio-venancio/src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div>
+          <h1>
+            <Link href="/jogar">Quer Jogar?</Link>
+          </h1>
+        </div>
+
         <Image
           className="dark:invert"
           src="/next.svg"
