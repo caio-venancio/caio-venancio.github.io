a539f3d6506d98e5147e951e8aafe69eae7acf96
caio-venancio <caio.venancio784@gmail.com>
2025-09-09 21:26:46 -0300

    feat: add docs, css module

--- portifolio-venancio/src/app/page.tsx
+++ portifolio-venancio/src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Home() {
   return (
-    <div>
-      olá
-    </div>
+    <main className="flex min-h-screen flex-col p-6 bg-white">
+      <div className={styles.shape}></div>
+    </main>
   );
 }
