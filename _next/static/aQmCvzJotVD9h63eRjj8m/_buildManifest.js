2943ff80f5a0cf9187fd954277efa735f620d8aa
caio-venancio <caio-venancio@users.noreply.github.com>
2025-10-16 23:31:20 +0000

    deploy: 231291a3b990db2cba9978fb44ef99a94039df80

--- _next/static/aQmCvzJotVD9h63eRjj8m/_buildManifest.js
+++ _next/static/aQmCvzJotVD9h63eRjj8m/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/ff5ecd654cf5a4bc.js"
+    "./static/chunks/d40be74c5da852b7.js"
   ],
   "__rewrites": {
     "afterFiles": [],
---
+++ _next/static/chunks/3cdf9e1c31c90922.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/392a4d72a6c4495b.js",
+  "static/chunks/turbopack-63afa4f2f0dd8c94.js"
+])
